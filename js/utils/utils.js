5c3d3a885ef48a0759b46a026ebbf7a77028126b
marzuk <marzuk777@gmail.com>
2022-04-18 18:11:05 +0600

    validation added

--- js/utils/utils.js
+++ js/utils/utils.js
@@ -1,5 +1,58 @@
-let greet = name => console.log("Hello ", name);
+const greet = name => console.log("Hello ", name);
 
-let englishNameValidator = name => {
-    let pattern = /[a-zA-Z]/i;
+const isEnglishName = name => {
+    const pattern = /^[a-z]+([a-z ]?)+[a-z]+$/i;
+    return pattern.test(name);
+};
+
+const isEnglishNamePartial = name => {
+    const pattern = /^[a-z][a-z ]*$/i;
+    return pattern.test(name);
+};
+
+const isBengaliName = name => {
+    const pattern = /^([\u2437-\u2443\u2447-\u2448\u2451-\u2472\u2474-\u2480\u2482\u2486-\u2489\u2524-\u2525\u2527])+$/;
+    return pattern.test(name);
+};
+
+const isOnlyBengaliDigits = text => {
+    const pattern = /^[\u09E6-\u09EF]+$/;
+	return pattern.test(text);
+};
+
+const isOnlyEnglishDigits = text => {
+    const pattern = /^[0-9]+$/;
+	return pattern.test(text);
+};
+
+const containingBanla = text => {
+    const pattern = /^[\u981-\u09FE]+/;
+    return pattern.test(text);
+}
+
+const isValidEmail = email => {
+    const pattern = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,6}$/i;
+    return pattern.test(email);
+};
+
+const isValidBDPhone = phone => {
+    const pattern = /^((88)?|(\+88)?)01[1|3-9][0-9]{8}$/;
+    return pattern.test(phone);
+};
+
+const isValidPassword = password => {
+    const pattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{4,}$/;
+    return pattern.test(password);
+};
+
+const isValiduserName = userName => {
+    const pattern = /^[a-z]+\w{3,9}$/i;
+    return pattern.test(userName);
+};
+
+const ageCalculation = dob => {
+    let date = dob.split("/");
+    const formattedDOB = new Date(date[2], date[1] - 1, date[0]);
+    let today = new Date();
+    return Math.floor((today - formattedDOB) / (365.25 * 24 * 60 * 60 * 1000));
 };
--- js/validation/formValidator.js
+++ js/validation/formValidator.js
@@ -1,3 +1,155 @@
 $().ready(function(){
-    $(".btn").click(greet("Marzuk"));
+    const usersEnglishName_defaultText = $("#englishNameHelp").text();
+    const usersBanglaName_defaultText = $("#banglaNameHelp").text();
+    const phoneNumber_defaultText = $("#phoneNumberHelp").text();
+    const userName_defaultText = $("#userNameHelp").text();
+    const email_defaultText = $("#emailHelp").text();
+    const password_defaultText = $("#passwordHelp").text();
+
+    let formValue = {
+        englishName: {},
+        banglaName: {},
+        phoneNumber: {},
+        userName: {},
+        email: {},
+        password: {},
+        tncChecked: {}
+    };
+
+    $(".btn").click(function(event){
+       
+        let tncChecked = $("#tncCheck").is(":checked");
+        if(!tncChecked){
+            $("#tncCheck").addClass("is-invalid");
+            $("#form-check-label-id").text("You have to agree with terms & conditions.");
+            event.preventDefault();
+        }
+
+        let isValidForm = false;
+
+        Object.keys(formValue).forEach(item => item.valid ? isValidForm = true : isValidForm = false);
+
+        if(isValidForm) alert("Submit success");
+        else{
+            console.log("---------------");
+
+            event.preventDefault();
+        }
+    });
+
+    $("#usersEnglishName").keyup(function(){
+        let value = $("#usersEnglishName").val();
+        let flag = isEnglishNamePartial(value);
+        formValue.englishName.value = value;
+
+        if(!flag){
+            $("#usersEnglishName").addClass("is-invalid");
+            $("#englishNameHelp").text(banglaMessages.person_name);
+            $("#englishNameHelp").addClass("text-danger");
+            formValue.englishName.valid = false;
+            formValue.englishName.errorMessage = banglaMessages.person_name;
+        }else{
+            $("#usersEnglishName").removeClass("is-invalid");
+            $("#englishNameHelp").text(usersEnglishName_defaultText);
+            $("#englishNameHelp").removeClass("text-danger");
+            formValue.englishName.valid = true;
+        }
+    });
+
+    $("#usersBanglaName").keyup(function(){
+        let value = $("#usersBanglaName").val();
+        let flag = containingBanla(value);
+        formValue.banglaName.value = value;
+
+        if(!flag){
+            $("#usersBanglaName").addClass("is-invalid");
+            $("#banglaNameHelp").text(banglaMessages.bengali_only);
+            $("#banglaNameHelp").addClass("text-danger");
+            formValue.banglaName.valid = false;
+            formValue.banglaName.errorMessage = banglaMessages.bengali_only;
+
+        }else{
+            $("#usersBanglaName").removeClass("is-invalid");
+            $("#banglaNameHelp").text(usersBanglaName_defaultText);
+            $("#banglaNameHelp").removeClass("text-danger");
+            formValue.banglaName.valid = true;
+        }
+    });
+
+    $("#phoneNumber").keyup(function(){
+        let value = $("#phoneNumber").val();
+        let flag = isValidBDPhone(value);
+        formValue.phoneNumber.value = value;
+        
+        if(!flag){
+            $("#phoneNumber").addClass("is-invalid");
+            $("#phoneNumberHelp").text(banglaMessages.phone_number);
+            $("#phoneNumberHelp").addClass("text-danger");
+            formValue.phoneNumber.valid = false;
+            formValue.phoneNumber.errorMessage = banglaMessages.phone_number;
+
+        }else{
+            $("#phoneNumber").removeClass("is-invalid");
+            $("#phoneNumberHelp").text(phoneNumber_defaultText);
+            $("#phoneNumberHelp").removeClass("text-danger");
+            formValue.phoneNumber.valid = true;
+        }
+    });
+
+    $("#userName").keyup(function(){
+        let value = $("#userName").val();
+        let flag = isValiduserName(value);
+        formValue.userName.value = value;
+
+        if(!flag){
+            $("#userName").addClass("is-invalid");
+            $("#userNameHelp").text(banglaMessages.phone_number);
+            $("#userNameHelp").addClass("text-danger");
+            formValue.userName.valid = false;
+            formValue.userName.errorMessage = banglaMessages.phone_number;
+        }else{
+            $("#userName").removeClass("is-invalid");
+            $("#userNameHelp").text(userName_defaultText);
+            $("#userNameHelp").removeClass("text-danger");
+            formValue.userName.valid = true;
+        }
+    });
+
+    $("#email").keyup(function(){
+        let value = $("#email").val();
+        let flag = isValidEmail(value);
+        formValue.email.value = value;
+
+        if(!flag){
+            $("#email").addClass("is-invalid");
+            $("#emailHelp").text(banglaMessages.email);
+            $("#emailHelp").addClass("text-danger");
+            formValue.email.valid = false;
+            formValue.email.errorMessage = banglaMessages.email;
+        }else{
+            $("#email").removeClass("is-invalid");
+            $("#emailHelp").text(email_defaultText);
+            $("#emailHelp").removeClass("text-danger");
+            formValue.email.valid = true;
+        }
+    });
+
+    $("#password").keyup(function(){
+        let value = $("#password").val();
+        let flag = isValidPassword(value);
+        formValue.password.value = value;
+
+        if(!flag){
+            $("#password").addClass("is-invalid");
+            $("#passwordHelp").text(banglaMessages.valid_password);
+            $("#passwordHelp").addClass("text-danger");
+            formValue.password.valid = false;
+            formValue.password.errorMessage = banglaMessages.valid_password;
+        }else{
+            $("#password").removeClass("is-invalid");
+            $("#passwordHelp").text(password_defaultText);
+            $("#passwordHelp").removeClass("text-danger");
+            formValue.password.valid = true;
+        }
+    });
 });
